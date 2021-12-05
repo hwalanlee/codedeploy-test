eec18e6b0b3e2c52732b533688e24ebf5a7b3ca8
hwalanlee <hwalan.lee@bespinglobal.com>
2021-12-06 08:48:36 +0900

    yes

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ sudo yum -y install nodejs
 sudo yum -y install git         # install git
 sudo npm install forever -g     # install forever
 git clone https://github.com/hwalanlee/simple-nodejs.git    # clone repo
-sudo echo -e "\n/home/ec2-user/simple-nodejs/atbooting.sh" >> /etc/rc.local     # register launch program
+sudo echo -e "\n/home/ec2-user/simple-nodejs/atbooting.sh" >> /etc/rc.local     # register launch programeee
 sudo chmod +x /etc/rc.local
 sudo echo -e "\n[Install]\nWantedBy=multi-user.target" >> /lib/systemd/system/rc-local.service
 sudo systemctl enable rc-local.service
